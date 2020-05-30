21f64af91a0e65d72e5d8693f9928afde45f4b07
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-30 19:33:55 +0800

    添加两个函数，打算写加密和解密

--- src/solution/SubstitutionTable.java
+++ src/solution/SubstitutionTable.java
@@ -2,4 +2,21 @@ package solution;
 
 public class SubstitutionTable {
     
+    /**
+     * 加密
+     * @param plaintext 明文
+     * @return 密文
+     * */
+    public String encrypt(String plaintext) {
+        return "";
+    }
+
+    /**
+     * 解密
+     * @param ciphertext 密文
+     * @return 明文
+     * */
+    public String decrypt(String ciphertext) {
+        return "";
+    }
 }
