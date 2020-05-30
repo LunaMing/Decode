8323551b00d18e5f51e545a72c842a3ce8117b78
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-30 19:36:10 +0800

    添加代换表测试代码

---
+++ src/solution/SubstitutionTableTest.java
@@ -0,0 +1,28 @@
+package solution;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubstitutionTableTest {
+    SubstitutionTable substitutionTable;
+
+    @Before
+    public void setUp() throws Exception {
+        substitutionTable = new SubstitutionTable();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void encrypt() {
+    }
+
+    @Test
+    public void decrypt() {
+    }
+}
