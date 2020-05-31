1024b120419b1b499fb18c88cb13371fb4570451
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-31 12:56:52 +0800

    添加统计函数注释

--- src/solution/Frequency.java
+++ src/solution/Frequency.java
@@ -3,6 +3,13 @@ package solution;
 import java.util.HashMap;
 
 public class Frequency {
+    
+    /**
+     * 统计频率
+     *
+     * @param inputStr 输入字符串
+     * @return 每个字母的出现频率，是一个Character和Double的映射表
+     */
     public HashMap<Character, Double> calculate(String inputStr) {
         HashMap<Character, Double> characterHashMap = new HashMap<Character, Double>();
         return characterHashMap;
