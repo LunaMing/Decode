a846907bf140b5c6acc6b4615c8fe003ee889a6b
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-31 19:29:09 +0800

    添加文件读写测试

---
+++ test/uiTest/FileReadWriteTest.java
@@ -0,0 +1,29 @@
+package uiTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ui.FileReadWrite;
+
+import static org.junit.Assert.*;
+
+public class FileReadWriteTest {
+    FileReadWrite fileReadWrite;
+
+    @Before
+    public void setUp() throws Exception {
+        fileReadWrite = new FileReadWrite();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void readTxt() {
+    }
+
+    @Test
+    public void writeTxt() {
+    }
+}
