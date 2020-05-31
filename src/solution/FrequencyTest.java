976e8f9a38a7cafd4c07b77257a396889020a27a
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-31 13:56:35 +0800

    新建测试用例，测试aabc的统计频率

--- src/solution/FrequencyTest.java
+++ src/solution/FrequencyTest.java
@@ -1,22 +1,32 @@
 package solution;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class FrequencyTest {
+    Frequency frequency;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        frequency = new Frequency();
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
     }
 
     @Test
     public void calculate() {
+        String s = "aabc";
+        HashMap<Character, Double> hashMap = frequency.calculate(s);
+        Character key = 'a';
+        Double frequnce = 0.5;
+        Assert.assertEquals(frequnce, hashMap.get(key));
     }
 }
