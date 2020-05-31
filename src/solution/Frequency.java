99eb5b7e8abaa032f884c716cbf3612a89283e92
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-31 16:55:23 +0800

    把函数全部改为静态的

--- src/solution/Frequency.java
+++ src/solution/Frequency.java
@@ -9,7 +9,7 @@ public class Frequency {
      * @param inputStr 输入的字符串
      * @return 每个字母的出现次数，是一个Character和Integer的映射表
      */
-    public TreeMap<Character, Integer> countNum(String inputStr) {
+    public static TreeMap<Character, Integer> countNum(String inputStr) {
         TreeMap<Character, Integer> letterNumMap = new TreeMap<>();
 
         //遍历字符串，统计字符出现的次数
@@ -35,7 +35,7 @@ public class Frequency {
      * @param inputStr 输入字符串
      * @return 每个字母的出现频率，是一个Character和Double的映射表
      */
-    public TreeMap<Character, Double> countFrequency(String inputStr) {
+    public static TreeMap<Character, Double> countFrequency(String inputStr) {
         TreeMap<Character, Double> freqMap = new TreeMap<>();
         //统计次数
         TreeMap<Character, Integer> letterNum = countNum(inputStr);
@@ -55,9 +55,9 @@ public class Frequency {
      * @param inputStr 输入的字符串
      * @return 已经排好序的一个字母列表
      */
-    public List<Character> sort(String inputStr) {
+    public static List<Character> sort(String inputStr) {
         List<Character> characterList = new ArrayList<>();
-        TreeMap<Character, Integer> treeMap = this.countNum(inputStr);
+        TreeMap<Character, Integer> treeMap = countNum(inputStr);
         // 升序比较器
         Comparator<Map.Entry<Character, Integer>> valueComparator = (o1, o2) -> (o2.getValue() - o1.getValue());
         // map转换成list进行排序
