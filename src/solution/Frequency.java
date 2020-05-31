99e4292be040c16b616d684ea7ad6d96fe30901f
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-05-31 12:53:16 +0800

    新建函数，新建测试类

--- src/solution/Frequency.java
+++ src/solution/Frequency.java
@@ -1,5 +1,11 @@
 package solution;
 
+import java.util.HashMap;
+
 public class Frequency {
-    
+    public HashMap<Character, Double> calculate(String inputStr) {
+        HashMap<Character, Double> characterHashMap = new HashMap<Character, Double>();
+        return characterHashMap;
+    }
+
 }
