9c24d93132987404bf75c49132d2364c504657b1
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-06-01 18:08:16 +0800

    完成S初始置换

--- src/algorithm/RC4.java
+++ src/algorithm/RC4.java
@@ -1,13 +1,46 @@
 package algorithm;
 
-import java.util.ArrayList;
-
 public class RC4 {
     String key;
-    ArrayList<Character> SBox = new ArrayList<>();
+    char[] S;
 
     public RC4(String key) {
         this.key = key;
+        initSBox();
+    }
+
+    /**
+     * S的初始置换
+     */
+    private void initSBox() {
+        //初始化S
+        S = new char[256];
+        for (int i = 0; i < 256; i++) {
+            //每一个格子里面就先放自己的位置
+            S[i] = (char) i;
+        }
+
+        //初始化T
+        char[] T = new char[256];
+        //把Key放到T里面，循环反复地放，直到T放满256格为止。
+        for (int i = 0; i < 256; i++) {
+            //用整除的方式找到放T的位置
+            //比如key是abc，T[012]和T[345]就应该分别是abc；
+            // T[345] = key[012]
+            T[i] = key.charAt(i % key.length());
+        }
+
+        //打乱S
+        int j = 0;
+        for (int i = 0; i < 256; i++) {
+            //这里是打乱算法，用S和T来打乱S里面的字符排序
+            j = j + S[i] + T[i];
+            j %= 256;//防止数组越界
+            //swap S[i] S[j]
+            char temp = S[i];
+            S[i] = S[j];
+            S[j] = temp;
+        }
     }
 
     /**
